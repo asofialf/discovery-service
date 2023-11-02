87b55f31e1024789d3906a8ea78c5243b99692ef
Sofia Alfaro <alfaros.sofia@gmail.com>
2023-11-02 15:48:45 -0500

    Initial commit

---
+++ src/test/java/com/edu/pe/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.pe.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
